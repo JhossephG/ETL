fb990358f02685cfba0a81643031e69003f3cd1a
Jhosseph <jhossephgonzalez@gmail.com>
2024-05-18 22:31:20 -0300

    feat: Implementing html_collector with tests and http_requester_mock

--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -7,20 +7,9 @@ class HttpRequester:
         self.__url= 'https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
 
     def request_from_page(self) -> Dict[int, str]:
-        try:
-            response = requests.get(self.__url, timeout=10)  # Adding a timeout of 10 seconds
-            return {
-                "status_code": response.status_code,
-                "html": response.text
-            }
-        except requests.Timeout:
-            return {
-                "status_code": "Timeout",
-                "html": ""
-            }
-        except requests.RequestException as e:
-            return {
-                "status_code": "Error",
-                "html": str(e)
-            }
+        response = requests.get(self.__url)
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
         
